79e0d181cf9afe4e0b1c47b4e9c8e156d6ab4b09
‘admahood’ <admahood@gmail.com>
2018-06-21 22:38:28 +0000

    closer

--- scripts/cloudmask_script_adam_edits.R
+++ scripts/cloudmask_script_adam_edits.R
@@ -22,41 +22,55 @@ path_row_combo <- path_row_combos[1] # placeholder before loop
 year = years[1] # placeholder before loop
 dir.create("data/scrap")
 
-for(year in years){}
+for(year in years){
   system(paste0("aws s3 sync s3://earthlab-amahood/data/landsat/landsat_", year,
                 " data/ls5/"))
-
-  for(path_row_combo in path_row_combos){}
-    tar_path <- "/home/rstudio/wet_dry/data/ls5/"
-    tar_list <- Sys.glob(paste0(tar_path, "LT05", path_row_combo,"*.gz"))
-    
-    qas <- data.frame(filenames = NA, value = NA, goodpix = NA, i = NA)
-    for (i in 1:length(tar_list)) {
-      exdir <- paste0("data/scrap/",i)
-      dir.create(exdir)
-      untar(tar_list[i], exdir = exdir)
-      qas[i, 1] <- Sys.glob(paste0(exdir, "/*pixel_qa.tif"))
-      x<-freq(raster(Sys.glob(paste0(exdir, "/*pixel_qa.tif")))) %>% as_tibble()
-      qas[i, 2] <- x[1,1]
-      qas[i, 3] <- x[1,2]
-      qas[i, 4] <- i
+  prcs <- list.files("data/ls5") %>% substr(5,10) %>% table() %>% as.data.frame()
+  colnames(prcs) <- c("prc", "freq")
+  
+  
+  for(path_row_combo in prcs$prc){
+    if(prcs[prcs$prc == path_row_combo,]$freq > 1){
+      tar_path <- "/home/rstudio/wet_dry/data/ls5/"
+      tar_list <- Sys.glob(paste0(tar_path, "LT05", path_row_combo,"*.gz"))
+      
+      qas <- data.frame(filenames = NA, value = NA, goodpix = NA, i = NA)
+      for (i in 1:length(tar_list)) {
+        exdir <- paste0("data/scrap/",i)
+        dir.create(exdir)
+        untar(tar_list[i], exdir = exdir)
+        qas[i, 1] <- Sys.glob(paste0(exdir, "/*pixel_qa.tif"))
+        x<-freq(raster(Sys.glob(paste0(exdir, "/*pixel_qa.tif")))) %>% as_tibble()
+        qas[i, 2] <- x[1,1]
+        qas[i, 3] <- x[1,2]
+        qas[i, 4] <- i
+      }
+      qas <- dplyr::arrange(qas, desc(goodpix))
+      ordered_i <- qas$i
+      
+      tifs <- list()
+      for(i in 1:length(tar_list)){
+        tifs[[i]] <- Sys.glob(paste0("data/scrap/", i, "/*band*.tif"))
+      }
+      
+      bands <- list()
+      qa <- list()
+      masked <- list()
+      for(i in 1:length(tar_list)){
+        bands[[i]] <- stack(tifs[[ordered_i[i][1]]])
+        qa[[i]] <- raster(Sys.glob(paste0("data/scrap/",ordered_i[i][1],"/*pixel_qa.tif")))
+        masked[[i]] <- overlay(x <- bands[[i]], y = qa[[i]], fun = maskcreate) # this takes time
+      }
+      x <- crop(masked[[2]], masked[[1]])
+      final <- crop(masked[[1]], x)
+      compareRaster(final, x)
+      
+      
+      #brackets from beginning      
     }
-    qas <- dplyr::arrange(qas, desc(goodpix))
-    ordered_i <- qas$i
-    
-    tifs <- list()
-    for(i in 1:length(tar_list)){
-      tifs[[i]] <- Sys.glob(paste0("data/scrap/", i, "/*band*.tif"))
-    }
-    
-    bands <- list()
-    qa <- list()
-    masked <- list()
-    for(i in 1:length(tar_list)){
-      bands[[i]] <- stack(tifs[[ordered_i[i][1]]])
-      qa[[i]] <- raster(Sys.glob(paste0("data/scrap/",ordered_i[i][1],"/*pixel_qa.tif")))
-      masked[[i]] <- overlay(x <- bands[[i]], y = qa[[i]], fun = maskcreate) # this takes time
-    }
+  }
+  system("rm -r data/ls5/")
+}
 #create generic mask creation function for use in overlay (can be done with basic raster arithmetic but overlay is easier on memory)
 # functions should go in the beginning of the script( or in a separate functions file)
 maskcreate<- function(x, y){
@@ -64,13 +78,7 @@ maskcreate<- function(x, y){
   return(x)
 }
 
-#apply mask using overlay()
 
-june4_masked <- overlay(x = june4, y = pqa_june4, fun = ls5_maskcreate)
-july6_masked <- overlay(x = july6, y = pqa_july6, fun = ls5_maskcreate)
-
-plotRGB(june4_masked, stretch = "hist")
-plotRGB(july6_masked, stretch = "hist")
 
 #check to see if masked rasters have same extent
 
