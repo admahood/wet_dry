9a9d8b94fe87a54dfdcab84dac8dc3ead357c993
‘admahood’ <admahood@gmail.com>
2018-08-20 19:41:44 +0000

    playing with hyperparameter ting

--- scripts/best_hyperparameters_model_application.R
+++ scripts/best_hyperparameters_model_application.R
@@ -32,63 +32,64 @@ cores <- length(model_list)
 
 registerDoParallel(cores)
 
+# whole lotta stuff that takes a long time doesn't need to be parallelized
+
+file <- as.character(scene)
+t0 <- Sys.time()
+ter <- stack(list.files(local_terrain, full.names =T))
+ls5 <- stack(scene_full)
+names(ls5)<- c("sr_band1", "sr_band2","sr_band3", "sr_band4","sr_band5", "sr_band7")
+
+
+ter_c <-raster::resample(ter,ls5)
+system(paste("echo", "terrain cropped"))
+# now, make sure all the names of this stack match the names that go into the model and we're golden
+ls5$wetness <- wet5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
+ls5$brightness <- bright5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
+ls5$greenness <- green5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
+ls5$ndvi <- get_ndvi(ls5$sr_band3, ls5$sr_band4)
+ls5$savi <- get_savi(ls5$sr_band3, ls5$sr_band4)
+ls5$sr <- get_sr(ls5$sr_band3, ls5$sr_band4)
+ls5$evi <- get_evi(ls5$sr_band1, ls5$sr_band3, ls5$sr_band4)
+#ls5$satvi <- get_satvi(ls5$sr_band3, ls5$sr_band5, ls5$sr_band7)
+ls5$ndsvi <- get_ndsvi(ls5$sr_band3, ls5$sr_band5)
+
+ls5 <- stack(ls5, ter_c)
+
+names(ls5) <- c("sr_band1", "sr_band2", "sr_band3", "sr_band4", "sr_band5", "sr_band7", "wetness", "brightness", "greenness",  "ndvi", "savi", "sr", "evi",  #"satvi",
+                "ndsvi", "flowdir", "folded_aspect","elevation", "roughness", "slope", "tpi", "tri"
+) #names to match exactly with training data that goes into model. The order matters for these
+
+system(paste("echo", "stack created"))
+
+print(Sys.time()-t0)
+gc() # for saving memory
+          
+#only parallelize this part
 foreach(i = 1:length(model_list), 
-        .packages = 'raster') %dopar% {
-          
-          source("scripts/functions.R")      
-          file <- as.character(scene)
-          t0 <- Sys.time()
-          ter <- stack(list.files(local_terrain, full.names =T))
-          ls5 <- stack(scene_full)
-          names(ls5)<- c("sr_band1", "sr_band2","sr_band3", "sr_band4","sr_band5", "sr_band7")
-          filenamet <- paste0("data/results/", as.character(names(model_list[i])), "_",
-                              sub(pattern = ".tif", 
-                                  replacement = "model_results.tif", 
-                                  x = file, fixed = T)) 
-          
-          
-          
-          ter_c <-raster::resample(ter,ls5)
-          system(paste("echo", "terrain cropped"))
-          # now, make sure all the names of this stack match the names that go into the model and we're golden
-          ls5$wetness <- wet5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
-          ls5$brightness <- bright5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
-          ls5$greenness <- green5(ls5$sr_band1,ls5$sr_band2,ls5$sr_band3,ls5$sr_band4,ls5$sr_band5,ls5$sr_band7)
-          ls5$ndvi <- get_ndvi(ls5$sr_band3, ls5$sr_band4)
-          ls5$savi <- get_savi(ls5$sr_band3, ls5$sr_band4)
-          ls5$sr <- get_sr(ls5$sr_band3, ls5$sr_band4)
-          ls5$evi <- get_evi(ls5$sr_band1, ls5$sr_band3, ls5$sr_band4)
-          #ls5$satvi <- get_satvi(ls5$sr_band3, ls5$sr_band5, ls5$sr_band7)
-          ls5$ndsvi <- get_ndsvi(ls5$sr_band3, ls5$sr_band5)
-          
-          ls5 <- stack(ls5, ter_c)
-          
-          names(ls5) <- c("sr_band1", "sr_band2", "sr_band3", "sr_band4", "sr_band5", "sr_band7", "wetness", "brightness", "greenness",  "ndvi", "savi", "sr", "evi",  #"satvi",
-                          "ndsvi", "flowdir", "folded_aspect","elevation", "roughness", "slope", "tpi", "tri"
-          ) #names to match exactly with training data that goes into model. The order matters for these
-          
-          system(paste("echo", "stack created"))
-          
-          print(Sys.time()-t0)
-          gc() # for saving memory
-          frst <- model_list[[i]]
-          # now put a line to apply the model and write THAT as the raster and send it to s3 (and then delete the file) 
-          ls5_classed <- predict(ls5, frst, type = 'class', inf.rm = T, na.rm = T)
-          
-          system(paste("echo", "model applied"))
-          
-          
-          print(Sys.time()-t0) #checking elapsed time between creation of big stack and application of model
-          
-          writeRaster(ls5_classed, filename = filenamet, overwrite = T, progress = 'text')
-          
-          system(paste0("aws s3 cp ", filenamet, " s3://earthlab-amahood/data/ls5_mucc_2011/", substr(filenamet, 14, 63)))
-          system(paste("echo", "done"))
-          unlink(filenamet)
-          
-          #it appears the task 2 failed error is coming from the deletion of temp files. I commented it out for now - Dylan
-          #system("rm data/tmp/*") # so we're not filling up the hard drive (had to move this to the end because the model needs the stuff stored in temp directory - D)
-        }
+                  .packages = 'raster') %dopar% {         
+  filenamet <- paste0("data/results/", as.character(names(model_list[i])), "_",
+                      sub(pattern = ".tif", 
+                          replacement = "model_results.tif", 
+                          x = file, fixed = T)) 
+  frst <- model_list[[i]]
+  # now put a line to apply the model and write THAT as the raster and send it to s3 (and then delete the file) 
+  ls5_classed <- predict(ls5, frst, type = 'class', inf.rm = T, na.rm = T)
+  
+  system(paste("echo", "model applied"))
+  
+  
+  print(Sys.time()-t0) #checking elapsed time between creation of big stack and application of model
+  
+  writeRaster(ls5_classed, filename = filenamet, overwrite = T, progress = 'text')
+  
+  system(paste0("aws s3 cp ", filenamet, " s3://earthlab-amahood/data/ls5_mucc_2011/", substr(filenamet, 14, 63)))
+  system(paste("echo", "done"))
+  unlink(filenamet)
+  
+  #it appears the task 2 failed error is coming from the deletion of temp files. I commented it out for now - Dylan
+  #system("rm data/tmp/*") # so we're not filling up the hard drive (had to move this to the end because the model needs the stuff stored in temp directory - D)
+}
 
 #non looping attempt ------------ 
 # if you want to test a loop, just type i=1 in the console (or whatever your iterator is)
