f73f3a5e11f5dc2e3d09a00842c83bfa670043aa
‘admahood’ <admahood@gmail.com>
2018-08-23 23:22:43 +0000

    hopefully fixed the terrain for winnemucca

---
+++ scripts/terrain_cropping_alm.R
@@ -0,0 +1,28 @@
+library("raster")
+system("aws s3 sync s3://earthlab-amahood/data/terrain_gb data/terrain")
+
+ter_files <- list.files("data/terrain", full.names = T)
+ter_names <- list.files("data/terrain", full.names = F)
+
+s3_path <- "s3://earthlab-amahood/data/ls5_mucc_2011"
+local_path <- "data/ls5_mucc"
+
+system(paste0("aws s3 sync ", s3_path, " ", local_path))
+
+ls5_files <- list.files("data/ls5_mucc/", full.names = T)
+
+ls5 <-raster(ls5_files)
+t1 <- raster(ter_files[1])
+
+ne <- projectExtent(ls5, crs=crs(t1))
+res(ne) <- 30
+
+for (i in 1:length(ter_files)){
+  t <- raster(ter_files[i])
+  c <- crop(t)
+  rp <- projectRaster(c,ne)
+  rs <- resample(rp,ls5)
+  writeRaster(rs, paste0("data/mucc_ter/", ter_names[i]))
+  system(paste0("aws s3 cp data/mucc_ter/",ter_names[i], 
+                " s3://earthlab-amahood/data/terrain_mucc/",ter_names[i]))
+}
--- terrain_processing.R
+++
@@ -1,4 +0,0 @@
-# nts -- clip to extent of the great basin polygon, then make aspect, elevation, etc
-
-download.file("https://landfire.gov/bulk/downloadfile.php?FNAME=US_Topo_2010-US_DEM_Elevation.zip&TYPE=landfire",
-              destfile = "data/dem_cus.tif")
