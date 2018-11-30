3a2d4162da1a21330e1cb55a20faf504f77108e1
‘admahood’ <admahood@gmail.com>
2018-11-30 23:04:00 +0000

    terrain chipping

--- scripts/terrain_by_pathrow.R
+++ scripts/terrain_by_pathrow.R
@@ -7,9 +7,9 @@ system("aws s3 sync s3://earthlab-amahood/data/ecoregions /home/rstudio/wet_dry/
 system("aws s3 sync s3://earthlab-amahood/data/WRS2_paths/wrs2_asc_desc /home/rstudio/wet_dry/data/WRS2_paths/wrs2_asc_desc")
 
 ter_path <- "data/terrain" 
-ter_files <- list.files(ter_path)
+ter_files <- list.files(ter_path)[2:8] #don't need aspect
 
-
+dir.create("scrap")
 
 ter_crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 
@@ -34,13 +34,20 @@ for(i in 1:length(path_row)){
   P <- substr(path_row[i],1,2)
   R <- substr(path_row[i],3,4)
   scene <- dplyr::filter(scenes, PATH == P, ROW == R) %>%
-    st_buffer(dist = 10000)
+    st_buffer(dist = 20000)
   
   for(j in 1:length(ter_files)){
-    f <- paste0("scrap/p",P,"_r",R,"_",ter_files[j])
+    dir.create("scrap")
+    p <- "scrap/"
+    f <- paste0("p",P,"_r",R,"_",ter_files[j])
     r <- raster(file.path(ter_path,ter_files[j])) %>%
-      crop(scene, filename = f)
-    system(paste0("aws s3 cp "))
+      crop(scene, filename = paste0(p,f))
+    
+    system(paste0("aws s3 cp ",
+                  p,f,
+                  " s3://earthlab-amahood/data/terrain_by_pathrow/",
+                  P,R,"/",f))
+    unlink(paste0(p,f))
     
   }
 }
