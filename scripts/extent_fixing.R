6ab4c3c977a5a11e271c1cef093b89d2c89e5696
‘admahood’ <admahood@gmail.com>
2019-02-13 12:56:15 -0700

    extent fixing script good to go

--- scripts/extent_fixing.R
+++ scripts/extent_fixing.R
@@ -4,8 +4,16 @@ library(foreach)
 library(doParallel)
 
 corz <- detectCores()-1
-path <- "/home/a/data/landsat/p42r31"
-l_files <- list.files(path, full.names = T)
+root_path <- "/home/a/data/landsat/"
+scene_path<- "p42r31"
+local_path<-paste0(root_path, scene_path)
+s3_path<- "s3://earthlab-amahood/data/ls5_mucc/"
+
+extent_path<-paste0(root_path,"extents")
+
+system(paste("aws s3 sync ",s3_path, local_path))
+
+l_files <- list.files(local_path, full.names = T)
 
 test<- raster::stack(l_files) #failed!
 
@@ -19,20 +27,25 @@ for (j in 1:length(l_files)){
 }
 
 registerDoParallel(corz)
-foreach(i = 1:length(l_files))%dopar%{
-  ee<- extent(raster(l_files[1]))
-  for (j in 1:length(l_files)){
-    r<- raster(l_files[j])
-    if(extent(r)@xmin > ee@xmin) ee@xmin <- extent(r)@xmin
-    if(extent(r)@xmax < ee@xmax) ee@xmax <- extent(r)@xmax
-    if(extent(r)@ymin > ee@ymin) ee@ymin <- extent(r)@ymin
-    if(extent(r)@ymax < ee@ymax) ee@ymax <- extent(r)@ymax
-  }
+for(i in 1:length(l_files)){ #each iteration takes about 2 minutes
+  t0<- Sys.time()
+  
   rr <- raster::stack(l_files[i]) %>%
     crop(ee)
   writeRaster(rr, filename = l_files[i], overwrite =TRUE)
+  
+  # by default this only deletes files older than 24 hours(h)
+  raster::removeTmpFiles(h=0.001) 
   gc()
-  raster::removeTmpFiles()
-  system(paste("echo",i))
+  
+  system(paste("echo",i, Sys.time()-t0))
 }
 
+raster::stack(l_files) # works!!
+
+#now, create blank raster
+
+blank <- raster(l_files[1]) %>% 
+  setValues(0) %>%
+  writeRaster(filename = paste0(extent_path,"/trimmed_extent_",scene_path),
+              dType="LOG1S")
