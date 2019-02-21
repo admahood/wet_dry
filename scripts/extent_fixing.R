db79806b67bf50fbde6beb5834b6f223f441f194
= <=>
2019-02-21 23:27:06 +0000

    extents on s3 for real this time

--- scripts/extent_fixing.R
+++ scripts/extent_fixing.R
@@ -8,7 +8,7 @@ root_path <- "data/landsat/"
 scene_path<- "p42r31"
 local_path<-paste0(root_path, scene_path)
 s3_path<- "s3://earthlab-amahood/data/ls5_mucc/"
-s3_upload_path <- "s3://earthlab-amahood/data/ls5_mucc_extent_matched"
+s3_upload_path <- "s3://earthlab-amahood/data/ls5_mucc_extent_matched/"
 
 extent_path<-paste0(root_path,"extents")
 
@@ -33,27 +33,31 @@ for(i in 1:length(l_files)){ #each iteration takes about 2 minutes
   
   rr <- raster::stack(l_files[i]) %>%
     crop(ee)
-  writeRaster(rr, filename = l_files[i], overwrite =TRUE)
+  filename = paste0(substr(l_files[[i]], 0, 35), "_e.tif")
+  writeRaster(rr, filename = filename)
   
-  # by default this only deletes files older than 24 hours(h)
+  system(paste0("aws s3 cp ", filename,  " ", s3_upload_path))
+   # by default this only deletes files older than 24 hours(h)
   raster::removeTmpFiles(h=0.001) 
   gc()
   
   system(paste("echo",i, Sys.time()-t0))
+  raster::removeTmpFiles(h=0.001) 
+  gc()
 }
 
 test2 <- raster::stack(l_files) # works!!
 
 #now, create blank raster
 
-blank <- raster(l_files[1]) %>% 
+blank <- raster(filename) %>% 
   setValues(0) %>%
   writeRaster(filename = paste0(local_path,"/trimmed_extent_",scene_path),
               dType="LOG1S") 
 
 
 #uploading extent fixed ls5 rasters and template raster to s3
-system(paste0("aws s3 cp ", local_path,"/trimmed_extent_",scene_path, ".gri", " ", s3_upload_path))
+system(paste0("aws s3 cp ", local_path,"/trimmed_extent_",scene_path, ".gri"," ", s3_upload_path))
 
 for(i in 1:length(l_files)) { 
   system(paste0("aws s3 cp ", l_files[i], " ", s3_upload_path))
