5ca59fb9f5e7fb58d08acb76f3b801179c8621d1
= <=>
2019-03-29 22:38:17 +0000

    accuracy metrics table script almost working but cant get it out of R

---
+++ scripts/final_model_metrics_table.R
@@ -0,0 +1,15 @@
+library(knitr, dplyr)
+
+data <- read.csv("data/table_data/thesis_final_models_accuracy_table.csv", stringsAsFactors = F, header = T)
+data <- as.tibble(data) %>% dplyr::select(-X, -elevation, - oob_balanced, - ac_null, -ac_upper, -ac_lower, -mcnemar_p, -pos_pred_value, -neg_pred_value, -F1, -prevalence, -balanced_accuracy) 
+model_names <- c("Shrub Over-predictor", "Grass Over-predictor", "Balanced")
+
+hypergrid_metric_names <- c("mtry", "nodesize", "sc", "OOB error", "OOB error: grass", "OOB error: shrub", "Accuracy (validation)", "Kappa", "Accuracy p-value", "Sensitivity", "Specificity", "Precision", "Recall", "Detection Rate", "Detection Prevalence")
+digits <- c(1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
+for(i in 1:length(digits)) { 
+  data[,i] <- round(data[, i], digits[i])
+}
+data <- as.tibble(base::t(data))
+rownames(data) <- hypergrid_metric_names
+
+kable(data, col.names = model_names, row.names = T)
