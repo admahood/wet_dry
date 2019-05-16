ac9bbab257c20567cd990abb6f13fd79e79bb142
= <=>
2019-05-16 21:36:04 +0000

    annual precipitation anomaly rasters clipped to naip scene are all done and on s3. ready for use as predictor variables in a new model run

--- scripts/prism_resample_script.R
+++ scripts/prism_resample_script.R
@@ -1,3 +1,4 @@
+#### Load Packages
 libs <- c("randomForest", "tidyverse","sf", "foreach", "doParallel",
           "caTools", # for sample.split()
           "caret", # for confusionMatrix
@@ -9,25 +10,26 @@ libs <- c("randomForest", "tidyverse","sf", "foreach", "doParallel",
           "rfUtilities" # for multi.collinear()
 )
 
-lapply(libs, install.packages, character.only = TRUE, verbose = FALSE)
+#lapply(libs, install.packages, character.only = TRUE, verbose = FALSE)
 lapply(libs, library, character.only = TRUE, verbose = FALSE)
 
 
-
+#### Pull data from AWS
 system("aws s3 sync s3://earthlab-amahood/data/naip data/naip")
 system("aws s3 sync s3://earthlab-amahood/data/PRISM_precip_annual data/precip_annual")
 system("aws s3 sync s3://earthlab-amahood/data/ls5_mucc_2011 data/ls5")
 
+#### Naip scene for trimming
 naip <- raster("data/naip/m_4011703_ne_11_1_20100704.tif")
 # naip<-list()
 # naip[[1]] <- raster("data/naip/m_4011703_ne_11_1_20100704.tif") #wmuc
 # naip[[2]] <- raster("data/naip/n_4111761_nw_11_1_20060813.tif") #frank
 # naip[[3]] <- raster("data/naip/m_4111823_sw_11_1_20100628.tif")
 
+#### ls5 scene for projection/resolution matching while downsampling
 ls5 <- stack(list.files("data/ls5", full.names = T))
-#ls5 <- crop(ls5, naip)
 
-#reprojecting, cropping, resampling PRISM annual precip to 30m resolution for use as predictor variables
+#reprojecting, cropping, resampling PRISM annual precip to 30m resolution 
 prism <- list()
 folders <- list.files("data/precip_annual/PRISM_annual_precip", full.names = T)
 for (i in 1:length(folders)) {
@@ -35,4 +37,24 @@ for (i in 1:length(folders)) {
   prism_oneyr <- projectRaster(prism_oneyr, ls5, res = 30)
   prism_oneyr <- crop(prism_oneyr, naip)
   prism[i] <- prism_oneyr
+}
+
+#setup PRISM 30yr normals raster (reproject, resample, crop to NAIP scene)
+normals <- raster("data/precip_annual/PRISM_30yr_precip_normals/PRISM_ppt_30yr_normal_4kmM2_annual_bil.bil")
+normals <- projectRaster(normals, ls5, res = 30)
+normals <- crop(normals, naip)
+
+#create years iterator for file names
+years <- (1984:2011)
+
+#create directory to store precip anomaly rasters
+dir.create("data/precip_annual/naip_trimmed_anomaly")
+
+#loop over each annual precip raster and create precip anomaly rasters (annual deviation from 30yr normals)
+for (i in 1:length(prism)) {
+  filename <- paste0("data/precip_annual/naip_trimmed_anomaly/precip_anomaly_trimmed_", years[i], ".tif")
+  filename_short <- paste0("precip_anomaly_trimmed_", years[i], ".tif")
+  annual_precip_anomaly <- prism[[i]] - normals
+  writeRaster(annual_precip_anomaly, filename = filename)
+  system(paste0("aws s3 cp ", filename, " ", "s3://earthlab-amahood/data/PRISM_precip_annual/naip_trimmed_annual_precip_anomaly/", filename_short))
 }
