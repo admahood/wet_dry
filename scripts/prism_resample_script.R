16362037ffd75a3b6cdb91f975cf9e5426b17e1d
= <=>
2019-05-21 00:06:05 +0000

    training data has precip anomaly extracted

--- scripts/prism_resample_script.R
+++ scripts/prism_resample_script.R
@@ -61,44 +61,53 @@ for (i in 1:length(prism)) {
   system(paste0("aws s3 cp ", filename, " ", "s3://earthlab-amahood/data/PRISM_precip_annual/naip_trimmed_annual_precip_anomaly/", filename_short))
 }
 
-#### TRAINING DATA PRECIP ANOMALY EXTRACTION ####
+#### TRAINING DATA (2011-2015) PRECIP ANOMALY EXTRACTION ####
 #load in training data to attach precip anomaly
 #system("aws s3 sync s3://earthlab-amahood/data/data_splits data/data_splits")
 system("aws s3 cp s3://earthlab-amahood/data/plots_with_landsat_feb19.gpkg data/plot_data/plots_with_landsat.gpkg")
 system("aws s3 sync s3://earthlab-amahood/data/ecoregions /home/rstudio/wet_dry/data/ecoregions")
 
-#load normals raster & grab crs
+#### load normals raster & grab crs
 normals <- raster("data/precip_annual/PRISM_30yr_precip_normals/PRISM_ppt_30yr_normal_4kmM2_annual_bil.bil")
-crs <- crs(normals)
+ls5 <- stack(list.files("data/ls5", full.names = T))
+crs1 <- crs(normals)
+crs2 <- crs(ls5)
 
-#load in ecoregions, merge to Gr. Basin shapefile, set as a spatial object for crop (not sf object)
+#### load in ecoregions, merge to Gr. Basin shapefile, set as a spatial object for crop (not sf object)
 ecoregions <- st_read("data/ecoregions/NA_CEC_Eco_Level3.shp")
 great_basin <- subset(ecoregions, NA_L3NAME %in% c("Northern Basin and Range", 
                                                    "Central Basin and Range", 
                                                    "Snake River Plain"))
 great_basin <- st_union(great_basin)
-great_basin <- st_transform(great_basin, crs = crs)
+great_basin <- st_transform(great_basin, crs = crs1)
 great_basin <- as(great_basin, "Spatial")
 
-#crop normals to great basin extent
+#### crop normals to great basin extent
 normals_gb <- crop(normals, great_basin)
 
+####reproject normals to match ls5 resolution and crs
+normals_gb <- projectRaster(normals_gb, crs = crs2, res = 30)
 
-#create annual precipitation rasters for training years (2011-2015)
+####saving and uploading cropped and reprojected normals raster to s3
+writeRaster(normals_gb, filename = "data/precip_annual/gb_cropped_normals.tif")
+system("aws s3 cp data/precip_annual/gb_cropped_normals.tif s3://earthlab-amahood/data/PRISM_precip_annual/gb_cropped_normals.tif")
+
+#### create annual precipitation rasters for training years (2011-2015)
 trainprism <- list()
 folders <- list.files("data/precip_annual/PRISM_annual_precip_training", full.names = T)
 for (i in 1:length(folders)) {
   prism_oneyr <- raster(list.files(folders[i], pattern = "\\.bil$", full.names = T))
-  #prism_oneyr <- projectRaster(prism_oneyr, crs = crs, res = 30)
+  #prism_oneyr <- projectRaster(prism_oneyr, crs = crs2, res = 30)
   prism_oneyr <- crop(prism_oneyr, great_basin)
+  prism_oneyr <- projectRaster(prism_oneyr, crs = crs2, res = 30)
   trainprism[i] <- prism_oneyr
 }
 
-#create directory for storing training years precip anomaly rasters
+#### create directory for storing training years precip anomaly rasters
 dir.create("data/precip_annual/greatbasin_trimmed_anomaly_training")
-#make vector of training years for filenames
+#### make vector of training years for filenames
 train_years <- (2011:2015)
-#loop over each year's precipitation raster, create precip anomaly raster, save and upload to s3
+#### loop over each year's precipitation raster, create precip anomaly raster, save and upload to s3
 for (i in 1:length(trainprism)) {
   filename <- paste0("data/precip_annual/greatbasin_trimmed_anomaly_training/precip_anomaly_train_", train_years[i], ".tif")
   filename_short <- paste0("precip_anomaly_train", train_years[i], ".tif")
@@ -107,12 +116,31 @@ for (i in 1:length(trainprism)) {
   system(paste0("aws s3 cp ", filename, " ", "s3://earthlab-amahood/data/PRISM_precip_annual/greatbasin_trimmed_anomaly_training/", filename_short))
 }
 
-#load training data points
+####load training data points
 gbd <- st_read("data/plot_data/plots_with_landsat.gpkg", quiet=T) %>%
   filter(esp_mask == 1) 
 
-#extract normals to gb training data
+#### extract normals to gb training data
+
 gbd <- dplyr::mutate(gbd, normals = raster::extract(normals_gb, gbd))
 
 
+training_anomaly_paths <- list.files("data/precip_annual/greatbasin_trimmed_anomaly_training", full.names = T)
+training_anomaly <- list()
+for(i in 1:length(training_anomaly_paths)) {
+  training_anomaly[i] <- raster(training_anomaly_paths[i])
+}
 
+precip_anomaly_vec <- c()
+for(i in 1:nrow(gbd)) {
+  precip_year <- as.numeric(gbd[i,]$year)
+ precip_anomaly_vec[i] <- raster::extract(training_anomaly[[precip_year]], gbd[i,])
+}
+
+
+#### extract annual precip anomaly to gb training data
+gbd <- dplyr::mutate(gbd, precip_anomaly = precip_anomaly_vec)
+
+#### save new training data with precip as a csv and upload to s3
+write.csv(gbd, file = "data/gbd_train_w_precip_5_20_19.csv")
+system("aws s3 cp data/gbd_train_w_precip_5_20_19.csv s3://earthlab-amahood/data/PRISM_precip_annual/gbd_train_w_precip_5_20_19.csv")
