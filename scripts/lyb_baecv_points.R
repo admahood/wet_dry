f975719c970671de70c5ec858605b9d5a8c741b4
= <=>
2019-05-31 00:49:00 +0000

    lyb extracted for blm plots, next need to make a time series for each plot based on lyb and then extract annual landsat stuff to each point in the time series

--- scripts/lyb_baecv_points.R
+++ scripts/lyb_baecv_points.R
@@ -3,6 +3,7 @@ library(sf)
 library(raster)
 library(foreach)
 library(doParallel)
+library(dplyr)
 
 local_scrap<- "data/scrap/"
 dir.create("data")
@@ -15,7 +16,7 @@ gpkg_file <- "data/plot_data/plots_with_landsat.gpkg"
 
 baecv_crs <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 "
 
-dd <-st_read(gpkg_file)%>%
+dd <-st_read(gpkg_file) %>%
   dplyr::select(-sr_band1,
                 -mean_sr_band1,
                 -sr_band2,
@@ -31,7 +32,7 @@ dd <-st_read(gpkg_file)%>%
                 -sr_cloud_qa,
                 -ndvi, - evi, -savi, -sr, 
                 -greenness, -wetness, -brightness) %>%
-    dplyr::mutate(plot_year = as.numeric(as.character(dd$year)))
+    dplyr::mutate(plot_year = as.numeric(as.character(dd$year))) %>%
   st_transform(st_crs(baecv_crs))
 
 # change yr_samp to whatever the year of sampling
@@ -44,24 +45,32 @@ corz <- detectCores() /2
 registerDoParallel(corz)
 results <- list()
 counter <- 1
-foreach(yy = 1984:y_max)%dopar%{
-  s3_file<- paste0("s3://earthlab-ls-fire/v1.1/BAECV_",yy,"_v1.1_20170908.tar.gz")
-  local_file<-paste0(local_scrap, "BAECV_",yy,"_v1.1_20170908.tar.gz")
-  target_file<-paste0("BAECV_bc_",yy,"_v1.1_20170908.tif")
+for(i in 1:32) {
+  yy = 1984:y_max
+  s3_file<- paste0("s3://earthlab-ls-fire/v1.1/BAECV_",yy[i],"_v1.1_20170908.tar.gz")
+  local_file<-paste0(local_scrap, "BAECV_",yy[i],"_v1.1_20170908.tar.gz")
+  target_file<-paste0("BAECV_bc_",yy[i],"_v1.1_20170908.tif")
   system(paste("aws s3 cp", s3_file, local_file))
+  system(paste("echo", "baecv downloaded", yy[i]))
   system(paste("tar xvzf", local_file, "-C", local_scrap, target_file))
+  system(paste("echo", "baecv untarred", yy[i]))
+  system(paste0("rm ", local_file))
   
   bc_file <- paste0(local_scrap, target_file)
   bc<- raster(bc_file)
+  system(paste("echo", "baecv raster created", yy[i]))
   
-  results[[counter]] <- mutate(dd, burned = raster::extract(bc,dd), # burned will be 0 or 1
-              burn_year = yy) # burn year will be year
+  
+  results[[i]] <- dd %>% mutate(burned = raster::extract(bc,dd), # burned will be 0 or 1
+              burn_year = yy[i]) # burn year will be year
+  
+  system(paste("echo", "burns extracted to training points", yy[i]))
   
   system(paste0("rm ", local_file))
   system(paste0("rm ", bc_file))
   gc()
   raster::removeTmpFiles(h=0.25)
-  counter <- counter+1
+  #counter <- counter+1
 }
 
 final <-do.call("rbind", results) %>%
@@ -71,5 +80,8 @@ final <-do.call("rbind", results) %>%
   group_by(plot_ID) %>%
   summarise(lyb = max(burn_year))
 
-st_write(final, "lyb_forthoseplots.gpkg")
-system("aws s3 cp ............")
+st_write(final, "lyb_gb_blm_plots.gpkg")
+system("aws s3 cp lyb_gb_blm_plots.gpkg s3://earthlab-amahood/data")
+
+final
+
