a2661eca5608a7434c275722f2a42a963f3a47f9
= <=>
2019-06-10 20:08:18 +0000

    landsat point extraction done, now to aggregate points with multiple scenes

--- scripts/timeseries_trainingdata_point_extraction.R
+++ scripts/timeseries_trainingdata_point_extraction.R
@@ -45,52 +45,63 @@ gb_plots$path_row <- as.character(paste0("0",gb_plots$PATH,"0",gb_plots$ROW))
 years <- unique(gb_plots$plot_year) # getting the years plots were monitored - 2011-2015
 path_row_combos <- unique(gb_plots$path_row)
 
+#grab list of landsat files in pixel replaced s3 bucket
 ls_files_list <- system("aws s3 ls s3://earthlab-amahood/data/landsat_5and7_pixel_replaced_Jun3/", intern = T)
 
+#subset landsat files down to just the filename (remove date of uploading etc)
 for(l in 1:length(ls_files_list)) {
   ls_files_list[l] <- substr(ls_files_list[l], 32, 51)
 }
 
+#### EXTRACT LANDSAT BAND VALUES TO POINTS - MODIFIED LOOP FROM WET_DRY_INITIAL_SCRIPT_SORTING_BY_DATE_FEB19 ####
+###It's finally working! Dylan - 6/10/19
 
+kounter = 1 #set counter to 1 at beginning of loop 
 
-gbplots_list <- list()
-
-
-
-#### modified loop from wet_dry_initial_script_sortingbydate_Feb19.R for landsat band extraction to points 
-###Issue with year. one year iteration works for all PRCs but then the loop fails. Dylan - 6/5/19
-counter = 1
-kounter = 1
+#Loop over each year and path/row combo and extract band values to points
 for(i in 1:length(years)){ 
+  
+  #Determine whether LS5 or LS7 file is needed for a particular year 
   if(years[i] >= 2011) { ls_platform <- 7 } else {ls_platform <- 5}
   print(paste0("platform selected", years[i]))
   
+  #Subset points with a specific path/row and year
   for(j in 1:length(path_row_combos)){
   gbplots_subset <- gb_plots[gb_plots$path_row == path_row_combos[j]
                              & gb_plots$plot_year == years[i],]
   print(paste0("plots subsetted for ", years[i], path_row_combos[j]))
-  print(paste(round(counter/125*100), "%")) #progress indicator
+  #print(paste(round(counter/125*100), "%")) #progress indicator
   
+  #select proper landsat scene from list of files in s3 bucket
   ls_files_annual <- str_subset(ls_files_list, pattern = fixed(years[i]))
   ls_files_annual <- str_subset(ls_files_annual, pattern = fixed(path_row_combos[j]))
+  ls_files_annual <- str_subset(ls_files_annual, pattern = fixed(paste0("ls", ls_platform)))
+  
+  #Download proper scene from s3
   system(paste0("aws s3 cp ", landsat_s3, ls_files_annual[1], " ", landsat_local, ls_files_annual[1]))
   print(paste0("landsat downloaded", years[i], path_row_combos[j]))    
-        # now we loop through each tif file and extract the values
-       
+  
+  
+  #create raster stack object from newly downloaded landsat scene & set variable names for each band    
   ls_stack <- raster::stack(paste0(landsat_local, "ls", ls_platform, "_", substr(ls_files_annual[1], 5, 20))) # this just loads the raster
   names(ls_stack) <- c("sr_band1", "sr_band2", "sr_band3", "sr_band4", "sr_band5", "sr_band7")
-        
+  
+  #check to make sure there are points for a particular pathrow/year      
   if(length(gbplots_subset$plot_year) > 0) { 
-    gbplots_subset <- raster::extract(ls_stack, gbplots_subset, sp=TRUE)
+  
+  #extract band values to subset of points
+  gbplots_subset <- raster::extract(ls_stack, gbplots_subset, sp=TRUE)
   print(paste0("band values extracted to points", years[i], path_row_combos[j])) 
+  
+  #transform points back to lat-long
   gbplots_subset <- st_transform(st_as_sf(gbplots_subset),crs = st_crs(plot_data))
   print(paste0("points transformed to lat long", years[i], path_row_combos[j]))    
   
-  file.remove(paste0(landsat_local, ls_files_annual)) # now we delete the tifs
+  #delete landsat scene from ec2 instance to save space
+  file.remove(paste0(landsat_local, ls_files_annual)) 
   print(paste0("landsat files deleted", years[i], path_row_combos[j]))  
   
-  # gbplots_list[[kounter]] <- gbplots_subset
-  
+  #combine points subset in dataframe with the rest of the points based on kounter value
   if(kounter == 1){
     result <- gbplots_subset
   }else{
@@ -98,16 +109,29 @@ for(i in 1:length(years)){
   }
   print(paste0("points attached to big training dataframe", years[i], path_row_combos[j])) 
   
+  #advance the counter value for the next loop iteration
   kounter <- kounter + 1
   } else {
+    
+  #if there are no points for a particular path/row & year, print "no points"
     print("no points")
   }
     }
-    counter <- counter + 1 
-  }
-  
-head(result)
+}
+#landsat bands to time series points extraction done!
 
+#### TAKING MEAN BAND VALUE FOR POINTS WHICH LIE IN MULTIPLE SCENES & COLLAPSING TO ONE POINT ####
+
+objectid_vec <- unique(result$OBJECTID)
+
+duplicated_vec <- duplicated(result$OBJECTID)
+
+
+for (i in 1:length(objectid_vec)) { 
+  objectid_group <- result[result$OBJECTID == objectid_vec[i],]
+  }
+
+subtest <- result[result$OBJECTID == objectid_vec[i],]
 
 ####MISSING LANDSAT FILES? START HERE####
 #find any missing landsat files
