2d6431f2aea964840be539aa8bd415d8ab5f90e5
= <=>
2019-12-05 21:04:00 +0000

    started script to create 30 yr z score precip rasters from prism data

---
+++ scripts/prism_precip_zscores.R
@@ -0,0 +1,23 @@
+#Title: Creating Annual Precipitation Z score Rasters vs 30 Yr Normals using PRISM
+#Author(s): Dylan Murphy
+#Date Created: Dec 5, 2019
+#Date Modified: Dec 5, 2019
+
+#### 1: Set Up ####
+
+#load packages
+library("raster", "dplyr", "stringr", "sf")
+
+#paths to files
+
+#aws s3 syncs
+
+#### 2: Data Prep #### 
+
+#### 3. Create Standard Deviation Rasters (from 30 yr. precip) ####
+
+#### 4. Create Z score Rasters ####
+
+
+
+
